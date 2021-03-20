03dbdd64e8a50a4a6705085a655e28e53c02b8ad
Synthesizer-source <56120358+Synthesizer-source@users.noreply.github.com>
2021-03-20 10:32:18 +0300

    added bottom navigation view

--- app/src/main/java/com/synthesizer/tmdbapp/MainActivity.java
+++ app/src/main/java/com/synthesizer/tmdbapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.synthesizer.tmdbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_second )
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
---
+++ app/src/main/java/com/synthesizer/tmdbapp/view/HomeViewModel.java
@@ -0,0 +1,7 @@
+package com.synthesizer.tmdbapp.view;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
